f2a071fe18ce2ec68389279bcc6a56cf44e63d44
Fernando Paucar <fernandopaucar149@gmai.com>
2022-06-22 15:45:45 -0500

    Implement search bar

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,33 @@
 import React from "react";
-import { Link } from "wouter";
+import { Link, useLocation } from "wouter";
 import "./styles.css";
+import { useState } from "react";
 
 const POPULAR_GIFS = ["Batman", "Goku", "Futbol", "Mario"];
 
 export const Home = () => {
+  const [keyword, setKeyword] = useState("");
+
+  const [location, setLocation] = useLocation();
+
+  console.log(location);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLocation(`/search/${keyword}`);
+  };
+
+  const handleChange = (e) => {
+    setKeyword(e.target.value);
+  };
+
   return (
     <>
       <div className="main-app">
+        <form action="" onSubmit={handleSubmit}>
+          <input onChange={handleChange} type="text" value={keyword} placeholder="Search a gif here ..."/>
+        </form>
+
         <ul className="enlaces">
           {POPULAR_GIFS.map((gif) => (
             <li key={gif}>
